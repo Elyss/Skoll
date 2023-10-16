6eb0df1c47326eb211a832adcf22bba8136e8f78
Elyss <florent.quijoux@gmail.com>
2023-10-16 13:56:17 +0200

    dernière v propre

--- masque_EME/forms.py
+++ masque_EME/forms.py
@@ -2,9 +2,6 @@ from django import forms
 from .models import Document
 
 class pre40(forms.ModelForm):
-   
-#    name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'Conseiller': 'nom du conseiller'}))
-
    class Meta:
         model = Document # Relie la classe pre40 au modèle Document dans mes modèles
         fields = ['pdf_file'] # Précise quels champs du modèle doivent être inclus à ce formulaire
